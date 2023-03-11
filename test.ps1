92458dc128b3fc602d12188204c35ccb49034686
Ben8565 <bcoquin.info@free.fr>
2023-03-11 08:54:13 +0100

    first commit

---
+++ test.ps1
@@ -0,0 +1,31 @@
+function Get-IniContent ($filePath)
+{
+    switch -regex -file $filePath
+    {
+        "^\[(.+)\]$"
+        {
+            $section = $matches[1]
+        }
+        "(.+)=(.*)"
+        {
+            $name, $value = $matches[1..2]
+            New-Object -TypeName PSObject -Property @{Section = $section
+                                    Parametre = $name
+                                    Valeur = $value
+                                    } | Select Section, Parametre, Valeur
+        }
+    }
+}
+$driveLabel = "EMTEC B111"
+$IniContent = Get-IniContent "USB2Letter.ini"
+#$Letter=$IniContent | Where { $_.Section -match "^$driveLabel$"} |  Where { $_.Parametre -match "^Letter$"} |sort-object section, parametre
+$Letter=$IniContent | Where { $_.Section -match "^$driveLabel$"} |  Where { $_.Parametre -match "^Letter$"}
+$Action=$IniContent | Where { $_.Section -match "^$driveLabel$"} |  Where { $_.Parametre -match "^Action$"} |sort-object section, parametre
+
+
+[System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+	
+# $oReturn=[System.Windows.Forms.Messagebox]::Show($Letter.Valeur)
+[System.Windows.Forms.Messagebox]::Show($Letter.Valeur)
+# $oReturn=[System.Windows.Forms.Messagebox]::Show($Action.Valeur)
+[System.Windows.Forms.Messagebox]::Show($Action.Valeur)
---
+++ usb_plug.ps1
@@ -0,0 +1,58 @@
+#Requires -version 2.0
+# il faut etre admin why 
+# schtasks /create /sc hourly /mo 1 /tn "MYTASK02" /tr "C:\test\test.bat" /st 00:00 /ru system /rl highest
+# /tn "MYTASK02"
+# schtasks /query /tn "MYTASK02"
+# schtasks /delete /tn "MYTASK02"
+# TODO : trace via log
+# TODO : paramtrage via fichier INI
+# TEST : cas ou le lecteur est deja monté
+
+Register-WmiEvent -Class win32_VolumeChangeEvent -SourceIdentifier volumeChange
+write-host (get-date -format s) " Beginning script..."
+do{
+    $newEvent = Wait-Event -SourceIdentifier volumeChange
+    $eventType = $newEvent.SourceEventArgs.NewEvent.EventType
+    $eventTypeName = switch($eventType)
+    {
+        1 {"Configuration changed"}
+        2 {"Device arrival"}
+        3 {"Device removal"}
+        4 {"docking"}
+    }
+    write-host (get-date -format s) " Event detected = " $eventTypeName
+    if ($eventType -eq 2)
+    {
+        $driveLetter = $newEvent.SourceEventArgs.NewEvent.DriveName
+        write-host (get-date -format s) " Drive name = " $driveLetter
+        start-sleep -seconds 1
+        $driveLabel = ([wmi]"Win32_LogicalDisk='$driveLetter'").VolumeName
+        write-host (get-date -format s) " Drive name = " $driveLetter
+        write-host (get-date -format s) " Drive label = " $driveLabel
+        # Execute process if drive matches specified condition(s)
+        # if ($driveLetter -eq 'Z:' -and $driveLabel -eq 'Mirror')
+        if ($driveLabel -eq 'TOOLC128GO')
+        {
+        write-host (get-date -format s) " Starting task in 1 seconds..."
+        start-sleep -seconds 1
+        # $driveLetter 
+        # Write-Host (-join($driveLetter, "\sync.bat")) 
+        #start-sleep -seconds 2
+        if ($driveLetter -ne 'S:')
+        {
+            # Changement de lecteur 
+            Write-Host (get-date -format s) "Changement de lecteur"
+            Get-CimInstance -Query "SELECT * FROM Win32_Volume WHERE Label='TOOLC128GO'" | Set-CimInstance -Arguments @{DriveLetter="S:"}
+            # Get-CimInstance -Query "SELECT * FROM Win32_Volume WHERE Label='$driveLabel'" | Set-CimInstance -Arguments @{DriveLetter="S:"}
+            #Get-Partition -DriveLetter $driveLetter | Set-Partition -NewDriveLetter "S:"
+        }
+        else 
+        {
+            write-host (get-date -format s) "Pas de  changement!"
+        }
+        # start-process (-join($driveLetter, "\sync.bat")) 
+        }
+    }
+    Remove-Event -SourceIdentifier volumeChange
+} while (1-eq1) #Loop until next event
+Unregister-Event -SourceIdentifier volumeChange
